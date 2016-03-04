177f70d09f2ab9fecbd98934f032ea9d82bfacce
tizz98 <dev.tizz98@gmail.com>
2016-03-04 11:06:55 -0500

    init

---
+++ app/src/main/java/org/zumh/android/sunset/SunsetActivity.java
@@ -0,0 +1,11 @@
+package org.zumh.android.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
